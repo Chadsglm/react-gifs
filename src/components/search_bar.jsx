7261c5bf6bcb8eebe5fac2161224db4a80c5abe3
Chad Saglam <saglam.chd@gmail.com>
2019-10-02 11:54:58 +0200

    created first app with giphy API

---
+++ src/components/search_bar.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  handleUpdate = (event) => {
+    this.props.searchFunction(event.target.value);
+  }
+
+  render() {
+    return (
+      <input
+        type="text"
+        className="form-control form-search"
+        onChange={this.handleUpdate}
+        value={event.target.value} // it's not working :(
+      />
+    );
+  }
+}
+
+export default SearchBar;
